be07ca5a84917a55398466cc71be9196dd0b1514
bear320 <aaabear320@gmail.com>
2022-11-19 13:12:29 +0800

    updated

---
+++ src/components/Images.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="img">
+        <Images :src="link" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        link: String,
+    },
+};
+</script>
+
+<style lang="scss">
+.img {
+    width: 100vw;
+    height: 10rem;
+    background-color: #999;
+}
+</style>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="news">
+        <Images
+            link="https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        />
+        <h1>This is a news page</h1>
+        <HelloWorld title="News Test" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import Images from "@/components/Images.vue";
+
+export default {
+    name: "NewsView",
+    components: {
+        HelloWorld,
+        Images,
+    },
+};
+</script>
